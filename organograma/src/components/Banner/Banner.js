93f94bf84ba5a0e859327b64e8060b1fe224ee34
MiguelZanela <57719624+MiguelZanela@users.noreply.github.com>
2022-06-29 22:16:04 -0300

    [New] Banner

---
+++ organograma/src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="the main banner of the organograma page"/>
+        </header>        
+    )
+}
+
+export default Banner
