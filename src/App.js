92f3f809b549606c4b88c7e944f53076b0df082e
MiguelZanela <57719624+MiguelZanela@users.noreply.github.com>
2022-07-03 15:43:44 -0300

    [New] Hooks to get the user inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Banner from './components/Banner'
-import Form from './components/Form'
+import { useState } from 'react';
+
+import Banner from './components/Banner';
+import Form from './components/Form';
 
 function App() {
+
+  const [employees, setEmployee] = useState([])
+
+  const toTheNewRegisteredEmployee = (employee) => {
+    console.log(employee)
+    setEmployee([...employees, employee])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Form />
+      <Form toTheRegisteredEmployee={employee => toTheNewRegisteredEmployee(employee)}/>
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 import './Form.css';
 import TextBox from '../TextBox'
 import DropDownList from '../DropDownList'
 import Button from '../Button';
 
-export const Form = () => {
+
+export const Form = (props) => {
 
     const teams = [
         '',
@@ -16,19 +19,54 @@ export const Form = () => {
         'Innovation and Management'
     ]
 
+    // Hooks, value field for read and setValue for set the value
+    // const [value, setValue] = useState('you can put any default value here')
+    const [name, setName] = useState('')
+    const [job, setJob] = useState('')
+    const [image, setImage] = useState('')
+    const [team, setTeam] = useState('')
+
     const whenSaving = (event) => {
         event.preventDefault()
-        console.log('Form submit')
+        props.toTheRegisteredEmployee({
+            name, // the same as  name: name
+            job, // the same as  job: job
+            image, // the same as  image: image
+            team // the same as  team: team
+        })
     }
 
     return (
         <section className="form">            
             <form onSubmit={whenSaving}>
                 <h2>Fill in the data to create the contributor card</h2>    	    
-                <TextBox mandatory={true} label="Name" placeholder="type your name" />
-                <TextBox mandatory={true} label="Job title" placeholder="enter your job title" />
-                <TextBox label="Image" placeholder="provide the URL of the image" />
-                <DropDownList mandatory={true} label="Teams" itens={teams} />
+                <TextBox 
+                    mandatory={true} 
+                    label="Name" 
+                    placeholder="type your name" 
+                    value={name}
+                    whenChanged={value => setName(value)}
+                />
+                <TextBox 
+                    mandatory={true} 
+                    label="Job title" 
+                    placeholder="enter your job title" 
+                    value={job}
+                    whenChanged={value => setJob(value)}
+                />
+                <TextBox 
+                    label="Image" 
+                    placeholder="provide the URL of the image" 
+                    value={image}
+                    whenChanged={value => setImage(value)}
+                />
+                <DropDownList 
+                    mandatory={true} 
+                    label="Teams" 
+                    itens={teams}
+                    value={team}
+                    whenChanged={value => setTeam(value)}
+                />
                 <Button>
                     Create Card
                 </Button>
